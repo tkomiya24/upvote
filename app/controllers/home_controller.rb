1b5c55c75dafebe5653026df3e57650a10268ed7
Takeru Komiya <tkomiya24@users.noreply.github.com>
2017-07-07 07:39:46 -0700

    Create a registration page (#4)

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ActionController::Base
-  layout 'application'
-
-  def index
-  end
-end
--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import Home from './Components/Home';
+import Registration from './Components/User/Registration';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import style from './css/styles.scss';
 
-ReactDOM.render(<Home />, document.getElementById('react-root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div className="application-container">
+      <Route exact path="/" component={Home} />
+      <Route path="/register" component ={Registration} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('react-root')
+);

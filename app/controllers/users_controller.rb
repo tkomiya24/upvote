55ed1034e0a305be274b9a3a883956e57936c0a2
Takeru Komiya <tkomiya24@gmail.com>
2017-07-15 18:11:35 -0700

    Fix token retrieval

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal:false
 
 require 'securerandom'
+require 'rest-client'
 
 class UsersController < ActionController::Base
   before_action :authenticate_user!
-  CLIENT_ID = ENV['CLIENT_ID']
   TYPE = 'code'.freeze
   URI = 'http://localhost:3000/users/reddit_authorized_callback'.freeze
   DURATION = 'permanent'.freeze
   SCOPE_STRING = 'history'.freeze
   AUTH_URL = 'https://www.reddit.com/api/v1/authorize'
-             .concat("?client_id=#{CLIENT_ID}&response_type=#{TYPE}")
+             .concat("?client_id=#{ENV['CLIENT_ID']}&response_type=#{TYPE}")
              .concat("&state=%s&redirect_uri=#{URI}")
              .concat("&duration=#{DURATION}&scope=#{SCOPE_STRING}")
+
+  ACCESS_TOKEN_URL = 'https://www.reddit.com/api/v1/access_token'.freeze
+  RestClient.log = Logger.new(STDOUT)
 
   def authorize_reddit
     random_string = SecureRandom.hex
@@ -27,7 +30,12 @@ class UsersController < ActionController::Base
     user = User.where(auth_string: params[:state]).first
     return handle_error unless params[:code]
     return handle_not_found unless user
-    if user.update(auth_code: params[:code], auth_string: nil)
+    begin
+      access_token = get_authorization_token(params[:code])
+    rescue RestClient::ExceptionWithResponse => e
+      return render(json: e.response)
+    end
+    if user.update(auth_code: access_token, auth_string: nil)
       redirect_to('/authorized')
     else
       render(json: { message: 'Could not update your user. Please try again later' }, status: 500)
@@ -35,6 +43,17 @@ class UsersController < ActionController::Base
   end
 
   private
+
+  def get_authorization_token(code)
+    payload = {
+      'grant_type' => 'authorization_code',
+      'code' => code,
+      'redirect_uri' => URI
+    }
+    resource = RestClient::Resource.new(ACCESS_TOKEN_URL, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
+    response = resource.post payload
+    JSON.parse(response.body)['access_token']
+  end
 
   def handle_error
     render(json: { message: 'There was an error trying to authorize your Reddit account' },
---
+++ src/Components/User/AuthError.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthError = () => {
+  return (
+    <div>
+      Your request to Reddit to authorize this app was denied.
+    </div>
+  );
+};
+
+export default AuthError;

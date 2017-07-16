74b0f5a4ca262a30ff5c30cc07f715810de6d02f
Takeru Komiya <tkomiya24@gmail.com>
2017-07-16 12:35:42 -0700

    Add Reddit data reducer to root reducer

--- src/Reducers/root.js
+++ src/Reducers/root.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 
 import UsersReducer from './user';
+import RedditDatumReducer from './RedditDatum';
 
 export default combineReducers({
-  users: UsersReducer
+  users: UsersReducer,
+  redditData: RedditDatumReducer
 });

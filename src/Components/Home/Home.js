6929193f1f86ef81d3dae07ebf8576bcffbd4434
Takeru Komiya <tkomiya24@gmail.com>
2017-07-22 15:09:09 -0700

    Move filter controls to upvotes#index component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,51 +8,21 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     if (props.user.reddit_username) {
-      props.getUpvotes().then(upvotes => {
-        this.setState({
-          filterValues: upvotes.map(upvote => {
-            return upvote.data.subreddit_name_prefixed;
-          }).filter((element, index, self) => {
-            return index === self.indexOf(element);
-          })
-        });
-      });
+      props.getUpvotes().then();
     }
-    this.state = {
-      filterValues: [],
-      subredditFilter: ''
-    };
-    this.changeFilterValue = this.changeFilterValue.bind(this);
   }
   render() {
     return (
       <div>
         <h1>{this.props.user.email}</h1>
-        <div className="filter-controls">
-          {this.state.filterValues.map(filterValue => {
-            return (
-              <button
-                key={filterValue}
-                onClick={() => { this.changeFilterValue(filterValue); }}>
-                {filterValue}
-              </button>
-            );
-          })}
-        </div>
         {this.props.user.reddit_username
           ? <Upvotes
-            subredditFilter={this.state.subredditFilter}
             upvotes={this.props.redditData} />
           : <a href="user/authorize_reddit">
             Authorize your reddit account.
           </a>}
       </div>
     );
-  }
-  changeFilterValue(filterValue) {
-    this.setState({
-      subredditFilter: filterValue
-    });
   }
 };
 
--- src/Components/Upvotes/index.js
+++ src/Components/Upvotes/index.js
@@ -2,20 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Show from './Show';
 
-const UpvotesIndex = ({upvotes, subredditFilter}) => {
-  upvotes = upvotes || [];
-  return (
-    <div>
-      {upvotes.map((upvote, index) => {
-        if (subredditFilter && subredditFilter !== upvote.data.subreddit_name_prefixed) {
-          return null;
-        }
-        return (
-          <Show key={index} upvote={upvote} />
-        );
-      })}
-    </div>
-  );
+class UpvotesIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterValues: [],
+      subredditFilter: ''
+    };
+    this.changeFilterValue = this.changeFilterValue.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.upvotes !== this.props.upvotes) {
+      this.setState({
+        filterValues: nextProps.upvotes.map(upvote => {
+          return upvote.data.subreddit_name_prefixed;
+        }).filter((element, index, self) => {
+          return index === self.indexOf(element);
+        })
+      });
+    }
+  }
+  render() {
+    const upvotes = this.props.upvotes || [];
+    return (
+      <div>
+        <div className="filter-controls">
+          {this.state.filterValues.map(filterValue => {
+            return (
+              <button
+                key={filterValue}
+                onClick={() => { this.changeFilterValue(filterValue); }}>
+                {filterValue}
+              </button>
+            );
+          })}
+        </div>
+        {upvotes.map((upvote, index) => {
+          if (this.props.subredditFilter &&
+              this.props.subredditFilter !== upvote.data.subreddit_name_prefixed) {
+            return null;
+          }
+          return (
+            <Show key={index} upvote={upvote} />
+          );
+        })}
+      </div>
+    );
+  }
+  changeFilterValue(filterValue) {
+    this.setState({
+      subredditFilter: filterValue
+    });
+  }
 };
 
 UpvotesIndex.propTypes = {

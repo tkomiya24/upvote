0568d1fa1bcb2a9579bdd53c077f1ac82231b6fb
Takeru Komiya <tkomiya24@users.noreply.github.com>
2017-07-24 20:49:31 -0700

    Fix error handling for reddit OAuth callback (#37)

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal:true
+
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test 'should redirect to auth error page if OAuth callback is called with unknown state' do
+    get reddit_authorized_callback_users_url, params: { code: '123', state: 'abc' }, xhr: true
+
+    assert_response :redirect
+    assert_redirected_to '/authorization_error'
+  end
+end
